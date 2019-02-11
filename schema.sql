37320982449affad5cfebd5ecb4bce1128831836
marina_cell <melissa_nsk_@mail.ru>
2019-02-11 16:25:01 +0700

    Создала схему БД для проекта

---
+++ schema.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE doingsdone
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+USE doingsdone;
+CREATE TABLE project (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       name CHAR(128)
+);
+CREATE TABLE task (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    date_create DATETIME,
+                    date_done DATETIME,
+                    state TINYINT DEFAULT 0,
+                    name CHAR(128),
+                    file CHAR,
+                    deadline DATETIME
+);
+CREATE TABLE user (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    date_regist DATETIME,
+                    email CHAR(128) NOT NULL UNIQUE,
+                    name CHAR(128),
+                    password CHAR(64)
+);
+CREATE UNIQUE INDEX p ON project(name);
+CREATE UNIQUE INDEX u ON user(name);
+CREATE INDEX t ON task(name);
