a7f81a6da78fcd8198b990fc34232f72b06fba3c
marina_cell <melissa_nsk_@mail.ru>
2019-03-15 11:56:11 +0700

    Объединение задач для одного пользователя в одно письмо.

--- notify.php
+++ notify.php
@@ -14,14 +14,29 @@ $mailer->registerPlugin(new Swift_Plugins_LoggerPlugin($logger));
 
 $tasks_todo = get_tasks_for_today($link);
 
-foreach ($tasks_todo as $task) {
-    $recipient[$task['email']] = $task['user_name'];
+$users_with_tasks = [];
+foreach($tasks_todo as $key => $val) {
+    $users_with_tasks[$val['user_id']][] = $val;
+}
 
+foreach ($users_with_tasks as $key => $row) {
     $message = new Swift_Message();
     $message->setSubject("Уведомление от сервиса «Дела в порядке»");
-    $message->setBody("Уважаемый(ая) " . $task['user_name'] . "! У Вас запланирована задача: '" . $task['task_name'] . "' на " . $task['deadline'] . ".");
-    $message->setFrom(['keks@phpdemo.ru' => 'DoingsDone']);
-    $message->setTo($recipient);
 
+    if (count($row) === 1) {
+        $text = "У Вас запланирована задача: '" . $row[0]['task_name'] . "' на " . $row[0]['deadline'];
+    }
+    else {
+        $list = [];
+        foreach ($row as $task) {
+            $list[] = "'" . $task['task_name'] . "' на " . $task['deadline'];
+        }
+        $list = implode(", ", $list);
+        $text = "У Вас запланированы задачи: " . $list;
+    }
+
+    $message->setBody("Уважаемый(ая) " . $row[0]['user_name'] . "! " . $text . ".");
+    $message->setFrom(['keks@phpdemo.ru' => 'DoingsDone']);
+    $message->setTo([$row[0]['email'] => $row[0]['user_name']]);
     $mailer->send($message);
 }
